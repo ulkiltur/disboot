dbaa23228e053bc676d159ecb46a25fb3f1e9ad4
Moonhell <byteknown@gmail.com>
2025-11-14 00:36:40 +0100

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,18 +1,28 @@
-// commands.js
-
 export const ping = {
-  name: 'ping',
-  description: 'Replies with Pong!',
-  async execute(message, args) {
-    await message.reply('Pong!');
+  data: {
+    name: 'ping',
+    description: 'Replies with Pong!'
+  },
+  async execute(interaction) {
+    await interaction.reply('Pong!');
   }
 };
 
 export const say = {
-  name: 'say',
-  description: 'Repeats your message.',
-  async execute(message, args) {
-    if (!args.length) return message.reply('You need to provide a message!');
-    await message.channel.send(args.join(' '));
+  data: {
+    name: 'say',
+    description: 'Repeats your message',
+    options: [
+      {
+        name: 'text',
+        type: 3, // STRING
+        description: 'Text to repeat',
+        required: true
+      }
+    ]
+  },
+  async execute(interaction) {
+    const text = interaction.options.getString('text');
+    await interaction.reply(text);
   }
 };
